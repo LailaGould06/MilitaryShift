1b999fc16e78d300d6a054e977ee01d7f80fda10
Laila Gould <lailagould10@gmail.com>
2023-07-19 11:33:26 -0500

    added exclamation point

--- SecondView.swift
+++ SecondView.swift
@@ -15,7 +15,7 @@ struct SecondView: View {
                 VStack(alignment:.center,spacing:10.0) {
                     Image("MS2").resizable().aspectRatio(contentMode: .fit).padding(/*@START_MENU_TOKEN@*/.horizontal, 102.0/*@END_MENU_TOKEN@*/).cornerRadius(200)
                     Spacer()
-                    Text ("Welcome \(name)")
+                    Text ("Welcome \(name)!")
                     NavigationLink(destination:Branches()) {
                         Text("Branches").font(.largeTitle).fontWeight(.light).foregroundColor(Color.white).padding()
                         
